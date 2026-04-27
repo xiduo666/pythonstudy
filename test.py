321ff186fdce68ead08861ec0acc8a968c57bf3c
xiduo666 <2504340326@wtu.edu.cn>
2026-04-27 23:08:06 +0800

    filter:补充笔记filter延迟绑定的陷阱

--- test.py
+++ test.py
@@ -1,5 +1,18 @@
 from functools import partial
-def log(level, message): print(f"[{level}] {message}")
-info_log=partial(log,level='INFO')
-error_log=partial(log,'ERROR')
-print(error_log('hello'))
+
+def iter():
+    n=2
+    while True:
+        yield n
+        n+=1
+
+def isprime(n):
+    it=iter()
+    u=next(it)
+    while u<n:
+        yield u
+        it=filter(partial(lambda x,p:x%p>0,p=u),it)
+        u=next(it)
+
+for i in isprime(10):
+    print(i)
